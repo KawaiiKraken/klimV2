0767bfb003d496973eb85cb6643d73e254cbb5cf
KawaiiKraken <adroit_kreken@proton.me>
2023-10-02 18:53:43 -0400

    reorganizing...

---
+++ helperFunctions.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <psapi.h>
+#include "isD2Active.h"
+
+const wchar_t* GetFileName( const wchar_t *path )
+{
+    const wchar_t *filename = wcsrchr( path, '\\' );
+    if ( filename == NULL )
+        filename = path;
+    else
+        filename++;
+    return filename;
+}
+
+bool isD2Active()
+{
+    TCHAR buffer[MAX_PATH] = {0};
+    DWORD dwProcId = 0; 
+    GetWindowThreadProcessId( GetForegroundWindow(), &dwProcId );
+    HANDLE hProc = OpenProcess( PROCESS_QUERY_INFORMATION | PROCESS_VM_READ , FALSE, dwProcId );
+    GetModuleFileNameEx( hProc, NULL, buffer, MAX_PATH );
+    printf( "buffer: %ls\n", buffer );
+    const wchar_t* bufferFilename = GetFileName( buffer );
+    printf( "filename: %ls\n", bufferFilename );
+    CloseHandle( hProc );
+    if ( wcscmp( bufferFilename, L"destiny2.exe" ) == 0 ){
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
---
+++ helperFunctions.h
@@ -0,0 +1,5 @@
+#pragma once
+
+
+bool isD2Active();
+const wchar_t* GetFileName( const wchar_t *path );
