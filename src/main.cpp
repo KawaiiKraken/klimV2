3da2ddb38aa3bf96f61e4df3fcc7230069aeec93
KawaiiKraken <111396148+KawaiiKraken@users.noreply.github.com>
2023-10-03 12:41:16 -0400

    reorganizing...

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,4 @@
 // TODO comments
-#define _WIN32_WINNT_WIN10 0x0A00 // Windows 10
-#define PHNT_VERSION PHNT_THRESHOLD // Windows 10
-                                    //
-#pragma comment( lib, "user32.lib" )
-#pragma comment( lib, "kernel32.lib" )
-#pragma comment( lib, "shell32.lib" )
-#pragma comment( lib, "advapi32.lib" )
-#pragma comment( lib, "Psapi.lib" )
-#pragma comment( lib, "ntdll.lib" )
-
-// these 2 are for the phnt header files
-#pragma clang diagnostic ignored "-Wpragma-pack"
-#pragma clang diagnostic ignored "-Wmicrosoft-enum-forward-reference"
-
-#pragma clang diagnostic ignored "-Wunused-variable"
-//#pragma clang diagnostic ignored "-Wwritable-strings"
-
-#include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <tchar.h>
-#include <stdbool.h>
-#include <iostream>
-#include <wchar.h>
-#include <shellapi.h>
-#include <psapi.h>
-
-#include "../phnt/phnt_windows.h"
-#include "../phnt/phnt.h"
-#include "../WinDivert/windivert.h"
-#include "helperFunctions.h"
 #include "main.h"
 
 using namespace std;
