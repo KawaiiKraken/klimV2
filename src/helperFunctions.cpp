035543a8bd82bbf6ac1229c8fbaea89145245c6b
KawaiiKraken <adroit_kreken@proton.me>
2023-11-24 18:34:51 -0500

    style changes

--- src/helperFunctions.cpp
+++ src/helperFunctions.cpp
@@ -4,8 +4,7 @@
 #include <fstream>
 #include "..\jsoncpp_x64-windows\include\json\json.h"
 
-const wchar_t* GetFileName( const wchar_t *path )
-{
+const wchar_t* GetFilename( const wchar_t *path ){
     const wchar_t *filename = wcsrchr( path, '\\' );
     if ( filename == NULL )
         filename = path;
@@ -14,102 +13,93 @@ const wchar_t* GetFileName( const wchar_t *path )
     return filename;
 }
 
-void StoreConfigToJson(wchar_t* filePath, const Json::Value& configData) {
-    // Open the file for writing
-    if (FileExists(filePath)) {
+void StoreConfigToJson( wchar_t* file_path, const Json::Value& config_data ){
+    if ( FileExists( file_path ) ){
         return;
     }
-    printf("creating new config file\n");
-    HANDLE hFile = CreateFileW( (LPCTSTR)filePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL );
+    printf( "creating new config file\n" );
+    HANDLE hFile = CreateFileW( ( LPCTSTR )file_path, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL );
 
-    if (hFile == INVALID_HANDLE_VALUE) {
-        // Handle error (you may want to add proper error handling)
-        printf("Error opening file for writing");
+    if ( hFile == INVALID_HANDLE_VALUE ){
+        printf( "Error opening file for writing" );
         return;
     }
 
-    // Convert Json::Value to string
-    std::string jsonData = configData.toStyledString();
+    std::string json_data = config_data.toStyledString();
 
-    // Write the JSON data to the file
     printf("settings config to default\n");
-    DWORD bytesWritten;
-    WriteFile(hFile, jsonData.c_str(), static_cast<DWORD>(jsonData.length()), &bytesWritten, NULL);
+    DWORD bytes_written;
+    WriteFile( hFile, json_data.c_str(), static_cast<DWORD>( json_data.length() ), &bytes_written, NULL );
 
-    // Close the file handle
-    CloseHandle(hFile);
+    CloseHandle( hFile );
 }
 
 
-bool isD2Active()
-{
+bool D2Active(){
     TCHAR buffer[MAX_PATH] = {0};
-    DWORD dwProcId = 0; 
+    DWORD dw_proc_id = 0; 
 
-    GetWindowThreadProcessId( GetForegroundWindow(), &dwProcId );
+    GetWindowThreadProcessId( GetForegroundWindow(), &dw_proc_id );
     
-    HANDLE hProc = OpenProcess( PROCESS_QUERY_INFORMATION | PROCESS_VM_READ , FALSE, dwProcId );
+    HANDLE hProc = OpenProcess( PROCESS_QUERY_INFORMATION | PROCESS_VM_READ , FALSE, dw_proc_id );
     GetModuleFileNameEx( hProc, NULL, buffer, MAX_PATH );
     CloseHandle( hProc );
 
-    const wchar_t* bufferFilename = GetFileName( buffer );
-    printf( "active window filename: %ls\n", bufferFilename );
+    const wchar_t* filename = GetFilename( buffer );
+    printf( "active window filename: %ls\n", filename );
 
-    if ( wcscmp( bufferFilename, L"destiny2.exe" ) == 0 ){
-        return TRUE;
+    if ( wcscmp( filename, L"destiny2.exe" ) == 0 ){
+        return true;
     } else {
-        return FALSE;
+        return false;
     }
 }
 
 
-// random TODO make hotkey systemw work with right and left keys
-void formatHotkeyStatusWcString( wchar_t* wcString, int szWcString, limit* limit){ 
-    char hotkeyBuffer[1] = { limit->hotkey };
-    char modkeyBuffer[1] = { limit->modkey };
+// random TODO make hotkey system work with right and left keys
+void FormatHotkeyStatusWcString( wchar_t* wcString, int szWcString, limit* limit ){ 
+    char hotkey_buffer[1] = { limit->hotkey };
+    char modkey_buffer[1] = { limit->modkey };
     
-    wchar_t* wcHotkeyBuffer = new wchar_t[2];
-    wchar_t* wcModkeyBuffer = new wchar_t[2];
-    size_t outSize;
+    wchar_t* wc_hotkey_buffer = new wchar_t[2];
+    wchar_t* wc_modkey_buffer = new wchar_t[2];
+    size_t out_size;
 
-    mbstowcs_s( &outSize, wcHotkeyBuffer, 2, hotkeyBuffer, 1);
-    mbstowcs_s( &outSize, wcModkeyBuffer, 2, modkeyBuffer, 1);
+    mbstowcs_s( &out_size, wc_hotkey_buffer, 2, hotkey_buffer, 1 );
+    mbstowcs_s( &out_size, wc_modkey_buffer, 2, modkey_buffer, 1 );
 
-    wcscpy_s(wcString, static_cast<rsize_t>(szWcString) - 1, wcModkeyBuffer);
-    //if ( limit->modkey == VK_SHIFT ){
+    wcscpy_s( wcString, static_cast<rsize_t>( szWcString ) - 1, wc_modkey_buffer );
     if ( limit->modkey == VK_LSHIFT ){
-        wcscpy_s( wcString, sizeof(L"shift"), L"shift" );
+        wcscpy_s( wcString, sizeof( L"shift" ), L"shift" );
     }
-    //if ( limit->modkey == VK_CONTROL ){
     if ( limit->modkey == VK_LCONTROL ){
-        wcscpy_s( wcString, sizeof(L"ctrl"), L"ctrl" );
+        wcscpy_s( wcString, sizeof( L"ctrl" ), L"ctrl" );
     }
-    //if ( limit->modkey == VK_MENU ){
     if ( limit->modkey == VK_LMENU ){
-        wcscpy_s( wcString, sizeof(L"alt"), L"alt" );
+        wcscpy_s( wcString, sizeof( L"alt" ), L"alt" );
     }
-    wcscat_s(wcString, szWcString, L"+");
-    wcscat_s(wcString, szWcString, wcHotkeyBuffer);
-    wcscat_s(wcString, szWcString, L" to ");
-    wcscat_s(wcString, szWcString, limit->name);
-    wcscat_s(wcString, szWcString, limit->state_name);
+    wcscat_s( wcString, szWcString, L"+" );
+    wcscat_s( wcString, szWcString, wc_hotkey_buffer );
+    wcscat_s( wcString, szWcString, L" to " );
+    wcscat_s( wcString, szWcString, limit->name );
+    wcscat_s( wcString, szWcString, limit->state_name );
 
-    delete []wcHotkeyBuffer;
+    delete []wc_hotkey_buffer;
 }
 
 
 
-bool IsElevated(){
+bool RunningAsAdmin(){
     bool fRet = false;
     HANDLE hToken = NULL;
-    if( OpenProcessToken( GetCurrentProcess(),TOKEN_QUERY,&hToken ) ) {
-        TOKEN_ELEVATION Elevation;
+    if( OpenProcessToken( GetCurrentProcess(), TOKEN_QUERY, &hToken ) ){
+        TOKEN_ELEVATION elevation;
         DWORD cbSize = sizeof( TOKEN_ELEVATION );
-        if( GetTokenInformation( hToken, TokenElevation, &Elevation, sizeof( Elevation ), &cbSize ) ) {
-            fRet = Elevation.TokenIsElevated;
+        if( GetTokenInformation( hToken, TokenElevation, &elevation, sizeof( elevation ), &cbSize ) ){
+            fRet = elevation.TokenIsElevated;
         }
     }
-    if( hToken ) {
+    if( hToken ){
         CloseHandle( hToken );
     }
     return fRet;
@@ -117,8 +107,7 @@ bool IsElevated(){
 
 
 
-bool FileExists( LPCTSTR szPath )
-{
+bool FileExists( LPCTSTR szPath ){
   DWORD dwAttrib = GetFileAttributes( szPath );
 
   return ( dwAttrib != INVALID_FILE_ATTRIBUTES && !( dwAttrib & FILE_ATTRIBUTE_DIRECTORY ) );
@@ -126,121 +115,112 @@ bool FileExists( LPCTSTR szPath )
 
 
 
-void writeDefaultJsonConfig(wchar_t* filePath) {
-    Json::Value configData;
-    configData["hotkey_exitapp"] = "k";
-    configData["modkey_exitapp"] = "ctrl";
-    configData["hotkey_3074"] = "g";
-    configData["modkey_3074"] = "ctrl";
-    configData["hotkey_3074UL"] = "c";
-    configData["modkey_3074UL"] = "ctrl";
-    configData["hotkey_27k"] = "6";
-    configData["modkey_27k"] = "ctrl";
-    configData["hotkey_27kUL"] = "7";
-    configData["modkey_27kUL"] = "ctrl";
-    configData["hotkey_30k"] = "l";
-    configData["modkey_30k"] = "ctrl";
-    configData["hotkey_7k"] = "j";
-    configData["modkey_7k"] = "ctrl";
-    configData["hotkey_game"] = "o";
-    configData["modkey_game"] = "ctrl";
-    configData["hotkey_suspend"] = "p";
-    configData["modkey_suspend"] = "ctrl";
-    configData["useOverlay"] = true;
-    configData["fontSize"] = 30;
-    configData["colorDefault"] = "0x00FFFFFF";
-    configData["colorOn"] = "0x000000FF";
-    configData["colorOff"] = "0x00FFFFFF";
+void WriteDefaultJsonConfig( wchar_t* filepath ){
+    Json::Value config;
+    config["hotkey_exitapp"] = "k";
+    config["modkey_exitapp"] = "ctrl";
+    config["hotkey_3074"] = "g";
+    config["modkey_3074"] = "ctrl";
+    config["hotkey_3074_ul"] = "c";
+    config["modkey_3074_ul"] = "ctrl";
+    config["hotkey_27k"] = "6";
+    config["modkey_27k"] = "ctrl";
+    config["hotkey_27k_ul"] = "7";
+    config["modkey_27k_ul"] = "ctrl";
+    config["hotkey_30k"] = "l";
+    config["modkey_30k"] = "ctrl";
+    config["hotkey_7k"] = "j";
+    config["modkey_7k"] = "ctrl";
+    config["hotkey_game"] = "o";
+    config["modkey_game"] = "ctrl";
+    config["hotkey_suspend"] = "p";
+    config["modkey_suspend"] = "ctrl";
+    config["use_overlay"] = true;
+    config["font_size"] = 30;
+    config["color_default"] = "0x00FFFFFF";
+    config["color_on"] = "0x000000FF";
+    config["color_off"] = "0x00FFFFFF";
 
-    StoreConfigToJson(filePath, configData);
+    StoreConfigToJson(filepath, config);
 }
 
 
 
-// Function to load configuration data from a JSON file
-Json::Value loadConfigFileFromJson(wchar_t* filePath) {
-    Json::Value jsonData;
+Json::Value LoadConfigFileFromJson( wchar_t* filepath ){
+    Json::Value json_data;
+    std::ifstream config_file( filepath, std::ifstream::binary );
 
-    // Open the file for reading
-    std::ifstream configFile(filePath, std::ifstream::binary);
-
-    if (configFile.is_open()) {
+    if ( config_file.is_open() ){
         Json::Reader reader;
+        bool parsingSuccessful = reader.parse(config_file, json_data);
 
-        // Read the JSON data from the file
-        bool parsingSuccessful = reader.parse(configFile, jsonData);
-
-        // Check if parsing was successful
-        if (!parsingSuccessful) {
-            // Handle error (you may want to add proper error handling)
+        if ( !parsingSuccessful ){
             std::cerr << "Error parsing JSON: " << reader.getFormattedErrorMessages() << std::endl;
         }
-
-        configFile.close();
+        config_file.close();
     } else {
-        // Handle error (you may want to add proper error handling)
         std::cerr << "Error opening file for reading" << std::endl;
     }
 
-    return jsonData;
+    return json_data;
 }
 
 
 
-void suspendProcess(DWORD pid) {
-    if ( pid != 0 ){
-        HANDLE procHandle = NULL;
-        procHandle = OpenProcess(PROCESS_SUSPEND_RESUME, 0, pid); 
-        if ( procHandle != NULL ){
-            printf("suspending process\n");
-            NtSuspendProcess(procHandle);
-            CloseHandle(procHandle);
-        }
+void SuspendProcess( DWORD pid ){
+    if ( pid == 0 ){
+        return;
+    }
+    HANDLE hProc = NULL;
+    hProc = OpenProcess( PROCESS_SUSPEND_RESUME, 0, pid ); 
+    if ( hProc != NULL ){
+        printf( "suspending process\n" );
+        NtSuspendProcess( hProc );
+        CloseHandle( hProc );
     }
 }
 
 
 
-void resumeProcess(DWORD pid){
-    if ( pid != 0 ){
-        HANDLE procHandle = NULL;
-        procHandle = OpenProcess(PROCESS_SUSPEND_RESUME, 0, pid); 
-        if ( procHandle != NULL ){
-            printf("resuming process\n");
-            NtResumeProcess( procHandle );
-            CloseHandle( procHandle );
-        }
+void ResumeProcess(DWORD pid){
+    if ( pid == 0 ){
+        return;
+    }
+    HANDLE hProc = NULL;
+    hProc = OpenProcess( PROCESS_SUSPEND_RESUME, 0, pid ); 
+    if ( hProc != NULL ){
+        printf( "resuming process\n" );
+        NtResumeProcess( hProc );
+        CloseHandle( hProc );
     }
 }
 
 
 
-void toggleSuspend( limit* suspend, COLORREF colorOn, COLORREF colorOff )
-{
+void ToggleSuspend( limit* suspend, COLORREF color_on, COLORREF color_off ){
     DWORD pid = 0;
     GetWindowThreadProcessId( GetForegroundWindow(), &pid );
-    if ( isD2Active() ){ // prevents from pausing random stuff if running with debug
-        suspend->toggleState();
+    if ( D2Active() ){ // prevents from pausing random stuff if running with debug
+        suspend->ToggleState();
 
         if ( suspend->state ){
-            suspendProcess(pid);
+            SuspendProcess( pid );
         } else {
-            resumeProcess(pid);
+            ResumeProcess( pid );
         }
 
-        COLORREF color = suspend->state ? colorOn : colorOff;
+        COLORREF color = suspend->state ? color_on : color_off;
         wchar_t* wcstring = new wchar_t[200];
-        formatHotkeyStatusWcString(wcstring, 200, suspend);
-        updateOverlayLine( wcstring, suspend->overlayLineNumber, color);
+        FormatHotkeyStatusWcString( wcstring, 200, suspend );
+        UpdateOverlayLine( wcstring, suspend->overlay_line_number, color );
         delete []wcstring;
     }
 }
 
 
 
-void toggleWholeGameLimit( limit* lim_game, COLORREF colorOn, COLORREF colorOff )
-{
-    lim_game->toggleState();
+void ToggleWholeGameLimit( limit* lim_game, COLORREF color_on, COLORREF color_off ){
+    lim_game->ToggleState();
 
     if ( lim_game->state ){
         ShellExecute( NULL, NULL, L"powershell.exe", L"-ExecutionPolicy bypass -noe -c New-NetQosPolicy -Name 'Destiny2-Limit' -AppPathNameMatchCondition 'destiny2.exe' -ThrottleRateActionBitsPerSecond 0.801KB", NULL, SW_HIDE );
@@ -248,62 +228,62 @@ void toggleWholeGameLimit( limit* lim_game, COLORREF colorOn, COLORREF colorOff 
         ShellExecute( NULL, NULL, L"powershell.exe", L"-ExecutionPolicy bypass -c Remove-NetQosPolicy -Name 'Destiny2-Limit' -Confirm:$false", NULL, SW_HIDE );
     }
 
-    COLORREF color = lim_game->state ? colorOn : colorOff;
+    COLORREF color = lim_game->state ? color_on : color_off;
     wchar_t* wcstring = new wchar_t[200];
-    formatHotkeyStatusWcString(wcstring, 200, lim_game);
-    updateOverlayLine( wcstring, lim_game->overlayLineNumber, color);
+    FormatHotkeyStatusWcString( wcstring, 200, lim_game );
+    UpdateOverlayLine( wcstring, lim_game->overlay_line_number, color);
     delete []wcstring;
 }
 
 
 
-void toggleBlockingLimit(limit* limit, COLORREF colorOn, COLORREF colorOff)
-{
-    limit->toggleState();
+void ToggleBlockingLimit( limit* limit, COLORREF color_on, COLORREF color_off ){
+    limit->ToggleState();
 
-    COLORREF color = limit->state ? colorOn : colorOff;
+    COLORREF color = limit->state ? color_on : color_off;
     wchar_t* wcstring = new wchar_t[200];
-    formatHotkeyStatusWcString( wcstring, 200, limit);
-    updateOverlayLine( wcstring, limit->overlayLineNumber, color);
+    FormatHotkeyStatusWcString( wcstring, 200, limit);
+    UpdateOverlayLine( wcstring, limit->overlay_line_number, color );
     delete []wcstring;
 }
 
-void setVarByKeyName(limit* limit, int* key, wchar_t* buffer) {
-    wchar_t* wcSingleChar = nullptr;
+
+
+void SetVarByKeyName( limit* limit, int* key, wchar_t* buffer ){
     // convert from key name to virtual keycode
-    if (wcscmp(buffer, L"") == 0) {
+    if ( wcscmp( buffer, L"") == 0 ){
         *key = 0x0;
-        printf("set %ls to: 0x0\n", limit->name);
-    } else 
-    if (wcscmp(buffer, L"alt") == 0) {
+        printf( "set %ls to: 0x0\n", limit->name );
+    } 
+    else if  ( wcscmp( buffer, L"alt" ) == 0 ){
         *key = VK_LMENU;
-        printf("set %ls to: alt\n", limit->name);
-    } else
-    if (wcscmp(buffer, L"shift") == 0) {
+        printf( "set %ls to: alt\n", limit->name );
+    } 
+    else if  ( wcscmp( buffer, L"shift" ) == 0 ){
         //*key = VK_SHIFT;
         *key = VK_LSHIFT;
-        printf("set %ls to: shift\n", limit->name);
-    } else
-    if (wcscmp(buffer, L"ctrl") == 0) {
+        printf( "set %ls to: shift\n", limit->name );
+    } 
+    else if ( wcscmp( buffer, L"ctrl" ) == 0 ){
         //*key = VK_CONTROL;
         *key = VK_LCONTROL;
-        printf("set %ls to: ctrl\n", limit->name);
+        printf ("set %ls to: ctrl\n", limit->name );
     } else {
-        wcSingleChar = &buffer[0];
-        *key = VkKeyScanW( *wcSingleChar );
-        printf( "set %ls to: %wc+%wc\n", limit->name, limit->hotkey, limit->modkey);
+        wchar_t* single_wc = nullptr;
+        single_wc = &buffer[0];
+        *key = VkKeyScanW( *single_wc );
+        printf( "set %ls to: %wc+%wc\n", limit->name, limit->hotkey, limit->modkey );
     }
 }
 
 
-void setVarFromJson(limit* limit, std::string hotkey, std::string modkey)
-{
-    const char* charPointer = hotkey.c_str();
-    const char* charPointer2 = modkey.c_str();
-    wchar_t buffer[20];
-    wchar_t buffer2[20];
-    MultiByteToWideChar(CP_UTF8, 0, charPointer, -1, buffer, 20);
-    MultiByteToWideChar(CP_UTF8, 0, charPointer2, -1, buffer2, 20);
-    setVarByKeyName(limit, &limit->hotkey, buffer);
-    setVarByKeyName(limit, &limit->modkey, buffer2);
+void SetVarFromJson( limit* limit, std::string hotkey, std::string modkey ){
+    const char* hotkey_char_ptr = hotkey.c_str();
+    const char* modkey_char_ptr = modkey.c_str();
+    wchar_t hotkey_buffer[20];
+    wchar_t modkey_buffer[20];
+    MultiByteToWideChar( CP_UTF8, 0, hotkey_char_ptr, -1, hotkey_buffer, 20 );
+    MultiByteToWideChar( CP_UTF8, 0, modkey_char_ptr, -1, modkey_buffer, 20 );
+    SetVarByKeyName( limit, &limit->hotkey, hotkey_buffer );
+    SetVarByKeyName( limit, &limit->modkey, modkey_buffer );
 }
--- src/helperFunctions.h
+++ src/helperFunctions.h
@@ -1,10 +1,6 @@
-#pragma once
 #include <windows.h>
 #include <tchar.h>
 #include <psapi.h>
-// these 2 are for the phnt header files
-//#pragma clang diagnostic ignored "-Wpragma-pack"
-//#pragma clang diagnostic ignored "-Wmicrosoft-enum-forward-reference"
 #include "../phnt/phnt_windows.h"
 #include "../phnt/phnt.h"
 #include "windivertFunctions.h"
@@ -18,26 +14,25 @@ struct limit {
     wchar_t state_name[20] = {'\0'};
     bool hotkey_down = false;
     bool modkey_down = false;
-    int overlayLineNumber = -1;
+    int overlay_line_number = -1;
     char windivert_rule[250];
-    void toggleState() {
+    void ToggleState() {
         state = !state;
         wcscpy_s(state_name, state ? (wchar_t*)L"(on)" : (wchar_t*)L"(off)");
     }
-    //limit(wchar_t* n, int i) : name(n), overlayLineNumber(i) {}
     limit(wchar_t* n) : name(n) {}
 };
 
-void formatHotkeyStatusWcString( wchar_t* wcstring, int szWcstring, limit* limit);
-bool isD2Active();
-bool IsElevated();
+void FormatHotkeyStatusWcString(wchar_t* wcstring, int szWcstring, limit* limit);
+bool D2Active();
+bool RunningAsAdmin();
 
-Json::Value loadConfigFileFromJson(wchar_t* filePath);
-void setVarFromJson(limit* limit, std::string hotkey, std::string modkey);
-void writeDefaultJsonConfig(wchar_t* filePath);
-bool FileExists( LPCTSTR szPath );
-const wchar_t* GetFileName( const wchar_t *path );
+Json::Value LoadConfigFileFromJson(wchar_t* filePath);
+void SetVarFromJson(limit* limit, std::string hotkey, std::string modkey);
+void WriteDefaultJsonConfig(wchar_t* filePath);
+bool FileExists(LPCTSTR szPath);
+const wchar_t* GetFilename(const wchar_t *path);
 
-void toggleBlockingLimit( limit* limit,     COLORREF colorOn, COLORREF colorOff);
-void toggleSuspend(       limit* suspend,   COLORREF colorOn, COLORREF colorOff );
-void toggleWholeGameLimit(limit* lim_game,  COLORREF colorOn, COLORREF colorOff );
+void ToggleBlockingLimit( limit* limit,     COLORREF colorOn, COLORREF colorOff);
+void ToggleSuspend(       limit* suspend,   COLORREF colorOn, COLORREF colorOff);
+void ToggleWholeGameLimit(limit* lim_game,  COLORREF colorOn, COLORREF colorOff);
