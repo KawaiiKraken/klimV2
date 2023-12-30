8c44862d3525b1d578e7432ecfc6c73e2173c9c6
WaifuShork <waifushork@gmail.com>
2023-12-30 10:58:50 -0500

    combined Limit and limit

--- src/Limit.h
+++ src/Limit.h
@@ -3,39 +3,35 @@
 #include <vector>
 #include <windows.h>
 
+class Limit
+{
+    public:
+        char name[40];
+        int key_list[20];
+        int max_key_list_size = 20;
+        char windivert_rule[250];
+        bool bindingComplete = true;
+        bool triggered = false;
+        bool state = false;
+        int overlay_line_number = -1;
+        bool updateUI = false;
 
-struct limit {
-    char name[40];
-    int key_list[20];
-    int max_key_list_size = 20;
-    char windivert_rule[250];
-    bool bindingComplete    = true;
-    bool triggered          = false;
-    bool state              = false;
-    int overlay_line_number = -1;
-    bool updateUI           = false;
+        Limit()
+        {
+            std::fill(std::begin(name), std::end(name), '\0');
+            std::fill(std::begin(windivert_rule), std::end(windivert_rule), '\0');
+            std::fill(std::begin(key_list), std::end(key_list), 0);
+        }
 
-    limit()
-    {
-        std::fill(std::begin(name), std::end(name), '\0');
-        std::fill(std::begin(windivert_rule), std::end(windivert_rule), '\0');
-        std::fill(std::begin(key_list), std::end(key_list), 0);
-    }
+        Limit(const char* n, const char* b = "") : Limit()
+        {
+            strncpy_s(name, n, sizeof(name));
+            strncpy_s(windivert_rule, b, sizeof(windivert_rule));
+        }
 
-    limit(const char* n, const char* b = "")
-        : limit()
-    {
-        strncpy_s(name, n, sizeof(name));
-        strncpy_s(windivert_rule, b, sizeof(windivert_rule));
-    }
-};
+        static void ToggleSuspend(std::atomic<Limit>* suspend);
+        static void ToggleWholeGameLimit(std::atomic<Limit>* lim_game);
 
-
-class Limit {
-public:
-    static void ToggleSuspend(std::atomic<limit>* suspend);
-    static void ToggleWholeGameLimit(std::atomic<limit>* lim_game);
-
-private:
-    static void SuspendProcess(DWORD pid, bool suspend);
+    private:
+        static void SuspendProcess(DWORD pid, bool suspend);
 };
