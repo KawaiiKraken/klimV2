0e23f814fc4ee1182121605298c3f3bd4593614f
KawaiiKraken <111396148+KawaiiKraken@users.noreply.github.com>
2024-01-01 20:29:00 -0500

    added visual config tab and fixed some bugs

--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -6,11 +6,13 @@
 #include "imgui/imgui_impl_opengl3.h"
 #include "imgui/imgui_impl_win32.h"
 #include <GL/GL.h>
+#include <dwmapi.h>
 #include <iostream>
 #include <string>
 #include <thread>
 #include <vector>
 #include <windows.h>
+#pragma comment(lib, "dwmapi.lib")
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -25,7 +27,7 @@ namespace Klim
 
     void UserInterface::FormatHotkeyStatusWcString(char* c_string, const std::atomic<Limit>* limit_ptr)
     {
-        // No keybinds set 
+        // No keybinds set
         if (limit_ptr->load().key_list[0] == 0)
         {
             return;
@@ -66,33 +68,22 @@ namespace Klim
 
     void UserInterface::Overlay(bool* p_open, const HWND window_handle) const
     {
-        static int corner = 0;
         const ImGuiIO& io = ImGui::GetIO();
-
-        if (corner != -1)
-        {
-            constexpr float distance = 10.0f;
-            const ImVec2 window_pos = ImVec2((corner & 1) ? io.DisplaySize.x - distance : distance, (corner & 2) ? io.DisplaySize.y - distance : distance);
-            const ImVec2 window_pos_pivot = ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
-            ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
-        }
 
         ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoResize;
 
         // make click-through (entire window)
         // TODO make it remove click-through when not needed
         SetWindowLongPtr(window_handle, GWL_EXSTYLE, GetWindowLongPtr(window_handle, GWL_EXSTYLE) | WS_EX_TRANSPARENT);
-        if (corner != -1)
-        {
-            window_flags |= ImGuiWindowFlags_NoMove;
-        }
 
         ImGuiStyle& style = ImGui::GetStyle();
 
         // Set background alpha (transparency)
         style.Colors[ImGuiCol_WindowBg].w = 0.0f;
         style.WindowBorderSize = 0.0f;
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
 
+        ImVec2 overlay_window_size(0.0f, 0.0f);
         if (ImGui::Begin("Overlay", p_open, window_flags))
         {
             std::vector<char[200]> char_ptr_vector(_limit_ptr_vector.size());
@@ -102,12 +93,19 @@ namespace Klim
                 ImGui::PushID(static_cast<int>(i));
                 if (strcmp(char_ptr_vector[i], "") != 0)
                 {
-                    std::vector<std::string> split = Helper::StringSplit(std::string(char_ptr_vector[i]), " ");
-                    ImGui::Text("%s %s %s", split[2].c_str(), split[0].c_str(), split[1].c_str());
+                    ImGui::Text(char_ptr_vector[i]);
+                    if (overlay_window_size.x < ImGui::CalcTextSize(char_ptr_vector[i]).x + 30)
+                    {
+                        overlay_window_size.x = ImGui::CalcTextSize(char_ptr_vector[i]).x;
+                        overlay_window_size.x += 30; // TODO replace with window style padding
+                    }
+                    overlay_window_size.y += ImGui::CalcTextSize(char_ptr_vector[i]).y;
+                    overlay_window_size.y += 10;
                 }
-
                 ImGui::PopID();
             }
+            ImGui::SetWindowSize(overlay_window_size);
+            SetWindowPos(window_handle, 0, 0, 0, overlay_window_size.x, overlay_window_size.y, SWP_NOZORDER | SWP_NOMOVE);
         }
 
         ImGui::End();
@@ -115,26 +113,78 @@ namespace Klim
 
     void UserInterface::Config(const HWND window_handle)
     {
-        // make click-through (entire window) 
+        // make click-through (entire window)
         // TODO make it remove click-through when not needed
         SetWindowLongPtr(window_handle, GWL_EXSTYLE, GetWindowLongPtr(window_handle, GWL_EXSTYLE) | ~WS_EX_TRANSPARENT);
 
+
+        static ImGuiWindowFlags flags = ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar;
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+        ImGui::SetNextWindowSize(ImVec2(250, 360));
+
+        ImGui::Begin("Config", nullptr, flags);
+        if (ImGui::BeginTabBar("MyTabBar"))
+        {
+            if (ImGui::BeginTabItem("Hotkeys"))
+            {
+                UserInterface::HotkeyTab();
+                ImGui::EndTabItem();
+            }
+
+            if (ImGui::BeginTabItem("Visuals"))
+            {
+                UserInterface::UiConfigTab(window_handle);
+                ImGui::EndTabItem();
+            }
+
+            ImGui::EndTabBar();
+        }
+
+        _window_size = ImGui::GetWindowSize();
+        SetWindowPos(window_handle, 0, 0, 0, _window_size.x, _window_size.y, SWP_NOZORDER | SWP_NOMOVE);
+
+        ImVec2 windowPos = ImGui::GetWindowPos();
+        ImVec2 windowSize = ImGui::GetWindowSize();
+        // Calculate the bottom boundary of the window
+        float windowBottom = windowPos.y + windowSize.y;
+        ImGui::SetCursorPosY(windowBottom - 30);
+        if (ImGui::Button("Close"))
+        {
+            // check if exit app is bound
+            for (std::atomic<Limit>*& limit_ptr : _limit_ptr_vector)
+            {
+                if (strcmp(limit_ptr->load().name, Limit::TypeToString(exit_app)) == 0)
+                {
+                    if (limit_ptr->load().key_list[0] == 0)
+                    {
+                        MessageBoxA(nullptr, "bind Exit App before closing", nullptr, MB_OK);
+                    }
+                    else
+                    {
+                        ConfigFile::WriteConfig(_limit_ptr_vector, _path_to_config_file, _settings);
+                        ConfigFile::LoadConfig(_limit_ptr_vector, _path_to_config_file, _settings);
+                        show_config = false;
+                        show_overlay = true;
+                    }
+                }
+            }
+        }
+
+        ImGui::End();
+    }
+
+    void UserInterface::HotkeyTab()
+    {
         std::vector<bool> button_clicked(_limit_ptr_vector.size());
         std::vector<std::string> string_vector(_limit_ptr_vector.size());
+
         int line_of_button_clicked = -1;
 
         for (size_t i = 0; i < _limit_ptr_vector.size(); i++)
         {
             string_vector.emplace_back("");
         }
-
-        static ImGuiWindowFlags flags = ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove;
-        ImGui::SetNextWindowPos(ImVec2(0, 0));
-        ImGui::SetNextWindowSize(ImVec2(250, 350));
-
-        ImGui::Begin("Config", nullptr, flags);
-
-        ImGui::SeparatorText("Hotkeys");
+        ImGui::SeparatorText("");
 
         float button_size = 0.0f;
         for (size_t i = 0; i < _limit_ptr_vector.size(); i++)
@@ -179,7 +229,6 @@ namespace Klim
 
             ImGui::Text("%s ", _limit_ptr_vector[i]->load().name); // Display some text (you can use a format strings too)
             ImGui::SameLine();
-            // ImGui::SetCursorPosX(70);
             ImGui::SetCursorPosX(115);
 
             const char* bind = string_vector[i].c_str();
@@ -236,35 +285,119 @@ namespace Klim
                 string_vector[i] = in_progress;
                 button_clicked[i] = false;
                 line_of_button_clicked = static_cast<int>(i); // this isn't required, but a static cast shows its intentional
-                std::thread([&]() { hk_instance->AsyncBindHotkey(line_of_button_clicked); }).detach();
+                bool can_trigger_hotkey = true;
+                for (int j = 0; j < _limit_ptr_vector.size(); j++)
+                {
+                    if (_limit_ptr_vector[j]->load().binding_complete == false)
+                    {
+                        can_trigger_hotkey = false;
+                    }
+                }
+                if (can_trigger_hotkey)
+                {
+                    std::thread([&]() { hk_instance->AsyncBindHotkey(line_of_button_clicked); }).detach();
+                }
             }
 
             ImGui::PopID();
         }
+    }
 
-        if (ImGui::Button("Close"))
-        {
-            // check if exit app is bound
-            for (std::atomic<Limit>*& limit_ptr : _limit_ptr_vector)
+    void UserInterface::HelpMarker(const char* description)
+    {
+        ImGui::Text("(?)");
+        if (ImGui::IsItemHovered())
+            if (ImGui::BeginItemTooltip())
             {
-                if (strcmp(limit_ptr->load().name, Limit::TypeToString(exit_app)) == 0)
-                {
-                    if (limit_ptr->load().key_list[0] == 0)
-                    {
-                        MessageBoxA(nullptr, "bind Exit App before closing", nullptr, MB_OK);
-                    }
-                    else
-                    {
-                        ConfigFile::WriteConfig(_limit_ptr_vector, _path_to_config_file, _settings);
-                        ConfigFile::LoadConfig(_limit_ptr_vector, _path_to_config_file, _settings);
-                        show_config = false;
-                        show_overlay = true;
-                    }
-                }
+                ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+                ImGui::TextUnformatted(description);
+                ImGui::PopTextWrapPos();
+                ImGui::EndTooltip();
             }
+    }
+
+    void UserInterface::UiConfigTab(const HWND hwnd)
+    {
+        ImGui::SeparatorText("Overlay");
+        {
+            static bool show_overlay = true;
+            ImGui::Checkbox("Show Overlay", &show_overlay);
+            ImGui::SameLine();
+            UserInterface::HelpMarker("Show/Hide overlay.");
+
+            static bool show_hotkey = true;
+            ImGui::Checkbox("Show hotkey", &show_hotkey);
+            ImGui::SameLine();
+            UserInterface::HelpMarker("Shows the hotkey to trigger a limit.");
+
+            static bool show_timer = true;
+            ImGui::Checkbox("Show timer", &show_timer);
+            ImGui::SameLine();
+            UserInterface::HelpMarker("Shows how long a limit has been on.");
+
+            static bool frosted_glass = false;
+            ImGui::Checkbox("Frosted glass", &frosted_glass);
+            if (ImGui::IsItemDeactivatedAfterEdit())
+            {
+                UserInterface::BlurBehindHwnd(hwnd, frosted_glass);
+            }
+            ImGui::SameLine();
+            UserInterface::HelpMarker("Frosted glass effect as background.");
+
+            static bool show_limit_state = true;
+            ImGui::Checkbox("Show limit state", &show_limit_state);
+            ImGui::SameLine();
+            UserInterface::HelpMarker("(on)/(off) text.");
+
+            static bool change_text_color = false;
+            ImGui::Checkbox("Change color", &change_text_color);
+            ImGui::SameLine();
+            UserInterface::HelpMarker("Change text color based on limit state.");
         }
 
-        ImGui::End();
+        ImGui::SeparatorText("Misc");
+        {
+            ImVec2 windowSize = ImGui::GetWindowSize();
+            ImVec2 maxWidgetSize = ImVec2(windowSize.x * 0.5f, windowSize.y * 0.5f);
+
+
+            ImGui::Text("Window position");
+            ImGui::PushID(0xdeadbeef); // doesn't work in this case unless given an id
+            const char* possible_window_pos[] = { "Top Left", "Top Right", "Bottom Left", "Bottom Right" };
+            ImGui::Combo("", &_window_location, possible_window_pos, IM_ARRAYSIZE(possible_window_pos));
+            ImGui::PopID();
+
+
+            ImGui::Text("Text size");
+            ImGui::SameLine();
+            UserInterface::HelpMarker("Currently broken");
+            ImGui::BeginDisabled();
+            ImGui::InputInt("", &font_size);
+            ImGui::EndDisabled();
+
+
+            ImGui::Text("Theme");
+            ImGui::SameLine();
+            UserInterface::HelpMarker("coming soon..?");
+        }
+    }
+
+
+    void UserInterface::SetHwndPos(HWND hwnd)
+    {
+        RECT desktop_rect;
+        const HWND desktop_window_handle = GetDesktopWindow();
+        GetWindowRect(desktop_window_handle, &desktop_rect);
+        const float PAD = 0.0f;
+        ImVec2 work_pos(0, 0); // Use work area to avoid menu-bar/task-bar, if any!
+        ImVec2 work_size(desktop_rect.right, desktop_rect.bottom);
+        ImVec2 window_pos, window_pos_pivot;
+        window_pos.x = (_window_location & 1) ? (work_pos.x + work_size.x - PAD - _window_size.x) : (work_pos.x + PAD);
+        window_pos.y = (_window_location & 2) ? (work_pos.y + work_size.y - PAD - _window_size.y) : (work_pos.y + PAD);
+        window_pos_pivot.x = (_window_location & 1) ? 1.0f : 0.0f;
+        window_pos_pivot.y = (_window_location & 2) ? 1.0f : 0.0f;
+
+        SetWindowPos(hwnd, 0, window_pos.x, window_pos.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
     }
 
 
@@ -350,7 +483,6 @@ namespace Klim
             style.WindowPadding = ImVec2(15.0f, 5.0f);
 
 
-            default_font->FontSize = 18.0f;
             ImGui::PushFont(default_font);
             if (show_config)
             {
@@ -358,13 +490,16 @@ namespace Klim
             }
             ImGui::PopFont();
 
-            custom_font->FontSize = 18.0f;
+            custom_font->FontSize = static_cast<float>(font_size);
             ImGui::PushFont(custom_font);
+            custom_font->FontSize = static_cast<float>(font_size);
             if (show_overlay)
             {
                 Overlay(&show_overlay, window_handle);
             }
             ImGui::PopFont();
+
+            UserInterface::SetHwndPos(window_handle);
 
             // Rendering
             ImGui::Render();
@@ -428,6 +563,59 @@ namespace Klim
         ReleaseDC(hWnd, data->device_context_handle);
     }
 
+    void UserInterface::BlurBehindHwnd(HWND hwnd, bool state)
+    {
+        enum AccentState
+        {
+            ACCENT_DISABLED = 0,
+            ACCENT_ENABLE_GRADIENT = 1,
+            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+            ACCENT_ENABLE_BLURBEHIND = 3,
+            ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
+            ACCENT_ENABLE_HOSTBACKDROP = 5, // RS5 1809
+            ACCENT_INVALID_STATE = 6
+        };
+        enum WindowCompositionAttribute
+        {
+            WCA_ACCENT_POLICY = 19
+        };
+        struct AccentPolicy
+        {
+                AccentState accentState;
+                int accentFlags;
+                int gradientColor;
+                int animationId;
+        };
+        struct WindowCompositionAttributeData
+        {
+                WindowCompositionAttribute attribute;
+                PVOID pData;
+                ULONG dataSize;
+        };
+        typedef BOOL(WINAPI * pSetWindowCompositionAttribute)(HWND, WindowCompositionAttributeData*);
+        const HINSTANCE hModule = LoadLibrary(TEXT("user32.dll"));
+        // int isRS4OrGreater = 1;
+        // int redValue = 0;
+        // int greenValue = 0;
+        // int blueValue = 0;
+        // int alphaValue = 20;
+        const pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(hModule, "SetWindowCompositionAttribute");
+        // int gradientColor = (alphaValue<<24) + (blueValue<<16) + (greenValue<<8) + (redValue);
+        // AccentState blurType = isRS4OrGreater == 1 ? ACCENT_ENABLE_ACRYLICBLURBEHIND : ACCENT_ENABLE_BLURBEHIND;
+        // AccentPolicy policy = {blurType, 2, gradientColor, 0};
+        AccentPolicy policy;
+        if (state)
+        {
+            policy = { ACCENT_ENABLE_ACRYLICBLURBEHIND, 2, 1, 0 };
+        }
+        else
+        {
+            policy = { ACCENT_DISABLED, 0, 0, 0 };
+        }
+        WindowCompositionAttributeData data = { WCA_ACCENT_POLICY, &policy, sizeof(AccentPolicy) };
+        SetWindowCompositionAttribute(hwnd, &data);
+    }
+
 
     // Win32 message handler
     // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
@@ -450,8 +638,8 @@ namespace Klim
             case WM_SIZE:
                 if (wParam != SIZE_MINIMIZED)
                 {
-                    ui_instance->_g_width = LOWORD(lParam);
-                    ui_instance->_g_height = HIWORD(lParam);
+                    // ui_instance->_g_width = LOWORD(lParam);
+                    // ui_instance->_g_height = HIWORD(lParam);
                 }
                 return 0;
             case WM_SYSCOMMAND:
